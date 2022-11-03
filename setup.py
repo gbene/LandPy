ebff1d5395ce6eb849a06eb490b0e62faacaf569
gbene <gabri.benedetti@gmail.com>
2022-11-03 11:43:22 +0100

    setup and init files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='landpy',
+    packages=find_packages(include=['landpy']),
+    version='0.1.0',
+    description='LandXML python parsing library',
+    author='Gabriele Benedetti',
+    license='MIT',
+)
