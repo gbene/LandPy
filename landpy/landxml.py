aa10a637c5d3060af8a6ff976e9adb07d1bfe710
gbene <gabri.benedetti@gmail.com>
2022-11-09 19:28:20 +0100

    class refactoring start (LandXMLSurfaces added)

--- landpy/landxml.py
+++ landpy/landxml.py
@@ -6,6 +6,7 @@ class LandXML:
     def __init__(self, lxml=None, *args, **kwargs):
         self.doc = et.XML(lxml)
         self.xmlns = str('{'+self.doc.nsmap[None]+'}')
+        self._surfaces = None
 
     def get_xmlns(self):
         return self.xmlns
@@ -47,27 +48,44 @@ class LandXML:
     @property
     def surfaces(self):
         surfs_list = []
-        surfs_ele = self.find_element('Surfaces') 
-        if surfs_ele == None:
+        surfaces = LandXMLSurfs(parent=self) 
+        return surfaces
+
+class LandXMLSurfs:
+    def __init__(self,parent = None,*args, **kwargs):
+        
+        self._surfs_ele = parent.find_element('Surfaces')
+        self._surfs_dict = {}
+        if self._surfs_ele is None:
             print('No surfaces in LandXML object')
         else:
-            children = surfs_ele.getchildren()
+            children = self._surfs_ele.getchildren()
             for child in children:
-                surf_name = child.attrib['name']
-                surf_def = child.find(f'{self.xmlns}Definition')
-                surf_type = child.find(f'{self.xmlns}Definition').attrib['surfType']
-                surf = LandXMLSurf(self,surf_name,surf_def,surf_type)
-                surfs_list.append(surf)
-            return surfs_list
+                surf = LandXMLSurf(parent=parent,surf_obj=child)
+                surf_name = surf.sname
+                self._surfs_dict[surf_name] = surf
+    
+    @property
+    def surfaces_ele(self):
+        return self._surfs_ele
+    
+    @surfaces_ele.setter
+    def surfaces_ele(self,lxml_ele):
+        self._surfs_ele = lxml_ele
+    
+    @property
+    def surfaces_dict(self):
+        return self._surfs_dict
 
 class LandXMLSurf:
     
-    def __init__(self,parent = None,surfName=None, surfDef=None, surfType=None, *args, **kwargs):
-
-        self._surfName = surfName
-        self._surfDef = surfDef
-        self._surfType = surfType
+    def __init__(self,parent = None, surf_obj = None,*args, **kwargs):
+        
         self.parent = parent
+        self._surfName = surf_obj.attrib['name']
+        self._surfDef = surf_obj.find(f'{parent.xmlns}Definition')
+        self._surfType = surf_obj.find(f'{parent.xmlns}Definition').attrib['surfType']
+        
     
     @property
     def sname(self):
@@ -150,3 +168,11 @@ class LandXMLSurf:
     
     def get_number_of_cells(self):
         return len(self.get_cells()[1])
+
+class LandXMLPoints:
+    def __init__(self, *args, **kwargs):
+        ...
+
+
+class LandXMLPoint:
+    ...
